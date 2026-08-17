313d30ef4c87dcaf0e5fc967e45507ff01b4b77b
jei36 <jei36bob@naver.com>
2026-08-17 13:21:31 +0900

    feat(access): detect missing privacy API access controls 2

--- privacyops-analyzer/target/test-classes/project-sample/src/main/java/com/example/MemberController.java
+++ privacyops-analyzer/target/test-classes/project-sample/src/main/java/com/example/MemberController.java
@@ -3,11 +3,15 @@ package com.example;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 @RestController
 @RequestMapping("/members")
 public class MemberController {
 
+    @PreAuthorize(
+            "hasRole('PRIVACY_HANDLER')"
+    )
     @GetMapping("/{id}")
     public MemberDto getMember() {
         return null;
